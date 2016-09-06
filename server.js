c5776403c520cfb31243aae9c04d4c9858d2504c
Chloebrz <chloe.brouzes@hotmail.fr>
2016-09-06 17:49:24 +0200

    Styles added. Deprecated items removed.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // dependencies
 var express = require("express");
 var routes = require("./routes");
+var bodyParser = require("body-parser");
+var errorHandler = require("errorhandler");
 
 var app = express();
 
@@ -10,20 +12,17 @@ app.engine("html", require("ejs").renderFile);
 app.set("port", process.env.PORT || 3000);
 app.set("views", __dirname + "/public/views");
 app.set("view engine", "html");
-app.use(express.bodyParser());
-app.use(express.methodOverride());
+app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
-app.use(app.router);
 
 if (app.get("env") === "development")
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  app.use(errorHandler({ dumpExceptions: true, showStack: true }));
 
 if (app.get("env") === "production")
-  app.use(express.errorHandler());
+  app.use(errorHandler());
 
 // Routes
 app.get("/", routes.index);
-//app.get("*", routes.index);
 
 app.listen(3000, function(){
   console.log("Express server listening on port 3000");
